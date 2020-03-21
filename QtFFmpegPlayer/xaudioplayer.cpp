c5c3b1158a2e75025c57967f5027e20dad02dfb9
i7-8700 <1281261856@qq.com>
2020-03-21 21:54:07 +0800

    xaudioplayer

---
+++ QtFFmpegPlayer/xaudioplayer.cpp
@@ -0,0 +1,96 @@
+#include "xaudioplayer.h"
+#include <QAudioFormat>
+#include <QAudioOutput>
+#include <mutex>
+
+class qaudioplayer : public xaudioplayer
+{
+public:
+	qaudioplayer() : xaudioplayer() {}
+	virtual ~qaudioplayer() {}
+
+	//! 打开音频播放
+	virtual bool open(int sampleRate = 44100, int sampleSize = 16, int channels = 2) override {
+		close();
+
+		QAudioFormat fmt;
+		fmt.setSampleRate(sampleRate);
+		fmt.setSampleSize(sampleSize);
+		fmt.setChannelCount(channels);
+		fmt.setCodec("audio/pcm");
+		fmt.setByteOrder(QAudioFormat::LittleEndian);
+		fmt.setSampleType(QAudioFormat::UnSignedInt);
+
+		std::lock_guard<std::mutex> lg(mutex);
+		sampleRate_ = sampleRate;
+		sampleSize_ = sampleSize;
+		channels_ = channels;
+		output = new QAudioOutput(fmt);
+		io = output->start();
+		return !!io;
+	}
+
+	virtual void close() override {
+		std::lock_guard<std::mutex> lg(mutex);
+		if (io) { io->close(); io = nullptr; }
+		if (output) {
+			output->stop();
+			delete output;
+			output = nullptr;
+		}
+	}
+
+	virtual void clear() override {
+		std::lock_guard<std::mutex> lg(mutex);
+		if (io) { io->reset(); }
+	}
+
+	//! 获取缓冲区中还没有播放的数据时长（毫秒）
+	virtual int64_t getNotPlayedMs() override {
+		std::lock_guard<std::mutex> lg(mutex);
+		if (!output) { return 0; }
+		// 还未播放的字节数
+		double size = output->bufferSize() - output->bytesFree();
+		// 一秒音频字节数
+		double oneSecSize = sampleRate_ * (sampleSize_ / 8) * channels_;
+		if (oneSecSize <= 0) { return 0; }
+		else { return size / oneSecSize * 1000; }
+	}
+
+	//! 获取可用缓冲区大小
+	virtual int getFreeSize() override {
+		std::lock_guard<std::mutex> lg(mutex);
+		if (!output) { return 0; }
+		return output->bytesFree();
+	}
+
+	//! 写入音频数据，需在getFreeSize返回大小不小于size时调用
+	virtual bool write(const char* data, int size) override {
+		if (!data || size <= 0) { return false; }
+		std::lock_guard<std::mutex> lg(mutex);
+		if (!output || !io) { return false; }
+		int writen = io->write(data, size);
+		return writen == size;
+	}
+
+	virtual void pause(bool isPause) override {
+		std::lock_guard<std::mutex> lg(mutex);
+		if (!output) { return; }
+		isPause ? output->suspend() : output->resume();
+	}
+
+private:
+	QAudioOutput* output{ nullptr };
+	QIODevice* io{ nullptr };
+	std::mutex mutex{};
+};
+
+
+xaudioplayer* xaudioplayer::instance()
+{
+	static qaudioplayer player;
+	return &player;
+}
+
+
+
---
+++ QtFFmpegPlayer/xaudioplayer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdint.h>
+
+class xaudioplayer
+{
+public:
+	static xaudioplayer* instance();
+
+	//! 打开音频播放
+	virtual bool open(int sampleRate = 44100, int sampleSize = 16, int channels = 2) = 0;
+	virtual void close() = 0;
+	virtual void clear() = 0;
+	//! 获取缓冲区中还没有播放的数据时长（毫秒）
+	virtual int64_t getNotPlayedMs() = 0;
+	//! 获取可用缓冲区大小
+	virtual int getFreeSize() = 0;
+	//! 写入音频数据，需在getFreeSize返回大小不小于size时调用
+	virtual bool write(const char* data, int size) = 0;
+	virtual void pause(bool isPause) = 0;
+
+protected:
+	xaudioplayer() {}
+	virtual ~xaudioplayer() {}
+
+	int sampleRate_ = 44100;
+	int sampleSize_ = 16;
+	int channels_ = 2;
+};
