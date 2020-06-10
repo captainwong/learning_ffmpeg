c897994840864644016455f54ce34cc68b7cd4df
i7-8700 <1281261856@qq.com>
2020-06-11 01:06:46 +0800

    update

--- mac_build_and_run_006.record_pcm.sh
+++ mac_build_and_run_006.record_pcm.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build/mac
 cd build/mac
-clang -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavcodec -lavdevice -lavformat -lavutil 
+clang++ -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavcodec -lavdevice -lavformat -lavutil 
 ./006.record_pcm
