0e9b309ec7cabfd6668e1c976b0a280f0af85743
i7-8700 <1281261856@qq.com>
2020-06-11 01:14:17 +0800

    update

--- linux_build_and_run_006.record_pcm.sh
+++ linux_build_and_run_006.record_pcm.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build/linux
 cd build/linux
-clang++ -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavcodec -lavdevice -lavformat -lavutil 
+g++ -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavcodec -lavdevice -lavformat -lavutil 
 ./006.record_pcm alsa hw:0 006.out.pcm
