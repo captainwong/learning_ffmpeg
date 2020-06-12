d2f0c42e163d78ebed95628421d2a26a90f29949
i7-8700 <1281261856@qq.com>
2020-06-12 17:33:21 +0800

    update

--- mac_build_and_run_006.record_pcm.sh
+++ mac_build_and_run_006.record_pcm.sh
@@ -3,9 +3,9 @@
 mkdir -p build/mac
 cd build/mac
 flags=`pkg-config --libs --cflags libavcodec libavdevice libavformat libavutil`
-build=`clang++ -std=c++11 -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp ${flags}`
-run=`./006.record_pcm avfoundation :0 006.out.pcm`
-
+#build=`clang++ -std=c++11 -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp ${flags}`
+#run=`./006.record_pcm avfoundation :0 006.out.pcm`
+rm -f 006.record_pcm
 clang++ -std=c++11 -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp ${flags} && ./006.record_pcm avfoundation :0 006.out.pcm
 #${build} && ${run}
 
