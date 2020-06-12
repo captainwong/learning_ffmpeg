d80d96efe6b5e8b5ed73a47e6279e6425356b939
i7-8700 <1281261856@qq.com>
2020-06-12 17:21:18 +0800

    update

--- mac_build_and_run_006.record_pcm.sh
+++ mac_build_and_run_006.record_pcm.sh
@@ -5,5 +5,5 @@ cd build/mac
 build=`clang++ -std=c++11 -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp -I/usr/local/ffmpeg/include -L/usr/local/ffmpeg/lib -lavcodec -lavdevice -lavformat -lavutil`
 run=`./006.record_pcm avfoundation :0 006.out.pcm`
 
-build && run
+${build} && ${run}
 
