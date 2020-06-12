3f293cc4536d9bb5310df1da7ce9c3845b44eb6c
i7-8700 <1281261856@qq.com>
2020-06-12 21:05:50 +0800

    update

--- 006.test_sample_format/006.test_sample_format.cpp
+++ 006.test_sample_format/006.test_sample_format.cpp
@@ -32,20 +32,34 @@ extern "C" {
 
 int main()
 {
+#ifdef _WIN32
+	const char* device = "dshow";
+	std::string audio_ = jlib::win32::mbcs_to_utf8("audio=麦克风 (Realtek High Definition Audio)");
+	const char* audio = audio_.data();
+#elif defined(__DARWIN__)
+	const char* device = "avfoundation";
+	const char* audio = ":0";
+#else
+#error todo
+#endif
+
 	avdevice_register_all(); // 这一句必须有，否则下面返回的 ifmt 是 NULL
-	AVInputFormat* ifmt = av_find_input_format("avfoundation");
+	AVInputFormat* ifmt = av_find_input_format(device);
 	if (!ifmt) {
-		fprintf(stderr, "Failed to find input format for input device 'avfoundation'\n");
+		fprintf(stderr, "Failed to find input format for input device '%s'\n", device);
 		return -1;
 	}
 	AVFormatContext* ic = NULL;
 	char msg[1024];
-	int ret = avformat_open_input(&ic, ":0", ifmt, NULL);
+	int ret = avformat_open_input(&ic, audio, ifmt, NULL);
 	if (ret < 0) {
 		av_strerror(ret, msg, sizeof(msg));
-		fprintf(stderr, "Failed to open audio device %s\n", msg);
+		fprintf(stderr, "Failed to open audio device '%s':%s\n", audio, msg);
 		return ret;
 	}
 
-	av_dump_format(ic, 0, ":0", 0);
+	av_dump_format(ic, 0, audio, 0);
+	avformat_close_input(&ic);
+
+	return 0;
 }
