9ed8716002a307be06b29b3c40bd1cd6fe39daba
i7-8700 <1281261856@qq.com>
2020-06-29 23:37:48 +0800

    fix

--- ffmpeg++/cmdutils.cpp
+++ ffmpeg++/cmdutils.cpp
@@ -295,7 +295,7 @@ int opt_timelimit(void* optctx, const char* opt, const char* arg)
 {
 #if HAVE_SETRLIMIT
     int lim = parse_number_or_die(opt, arg, OPT_INT64, 0, INT_MAX);
-    struct rlimit rl = rlimit{ lim, lim + 1 };
+    struct rlimit rl{ lim, lim + 1 };
     if (setrlimit(RLIMIT_CPU, &rl))
         perror("setrlimit");
 #else
