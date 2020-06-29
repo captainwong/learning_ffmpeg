10c5ee69ecd50b7629bcba3a77c13d2dcbc3002f
i7-8700 <1281261856@qq.com>
2020-06-29 21:32:09 +0800

    test ffmpeg++ on mac

---
+++ mac_build_and_run_ffmpeg++.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+mkdir -p build/mac
+cd build/mac
+flags=`pkg-config --libs --cflags libavcodec libavdevice libavfilter libavformat libavresample libavutil libpostproc libswresample libswscale`
+#build=`clang++ -std=c++11 -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp ${flags}`
+#run=`./006.record_pcm avfoundation :0 006.out.pcm`
+rm -f ffmpeg++
+folder='../../ffmpeg++'
+sources="${folder}/ffmpeg.cpp ${folder}/cmdutils.cpp ${folder}/ffmpeg_filter.cpp ${folder}/ffmpeg_hw.cpp ${folder}/ffmpeg_opt.cpp"
+clang++ -std=c++11 -o ffmpeg++ ${sources} -lpthread ${flags} && ./ffmpeg++
+#${build} && ${run}
+
