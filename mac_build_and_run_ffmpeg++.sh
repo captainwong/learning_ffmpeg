e3a8f4a089e87656e893e54747476ca41c6c3831
i7-8700 <1281261856@qq.com>
2020-06-29 22:04:45 +0800

    fix

--- mac_build_and_run_ffmpeg++.sh
+++ mac_build_and_run_ffmpeg++.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build/mac
 cd build/mac
-flags=`pkg-config --libs --cflags libavcodec libavdevice libavfilter libavformat libavutil libpostproc libswresample libswscale`
+flags=`pkg-config --libs --cflags libavcodec libavdevice libavfilter libavformat libavresample libavutil libpostproc libswresample libswscale`
 #build=`clang++ -std=c++11 -o 006.record_pcm ../../006.record_pcm/006.record_pcm.cpp ${flags}`
 #run=`./006.record_pcm avfoundation :0 006.out.pcm`
 rm -f ffmpeg++
