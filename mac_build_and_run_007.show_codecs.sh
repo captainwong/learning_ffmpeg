d197a1e560d611e96263347fe44a455afcda92c5
i7-8700 <1281261856@qq.com>
2020-07-05 22:27:24 +0800

    fix

--- mac_build_and_run_007.show_codecs.sh
+++ mac_build_and_run_007.show_codecs.sh
@@ -3,7 +3,8 @@
 mkdir -p build/mac
 cd build/mac
 flags=`pkg-config --libs --cflags libavcodec libavutil`
-folder='../../006.show_codecs'
-sources="${folder}/006.show_codecs"
-clang++ -o 006.show_codecs ${sources} -I${folder} ${flags} && ./006.show_codecs
+project='007.show_codecs'
+folder='../../${project}'
+sources="${folder}/${project}.cpp"
+clang++ -o ${project} ${sources} -I${folder} ${flags} && ./${project}
 
